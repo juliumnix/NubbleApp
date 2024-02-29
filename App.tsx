70dd34acd1bb200de0737a7a06fb669ae5f21b20
juliumnix <juliumnix@gmail.com>
2024-02-28 21:42:52 -0300

    dando suporte a icones com svg

--- App.tsx
+++ App.tsx
@@ -4,15 +4,14 @@ import {Text} from './src/components/Text/Text';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 import {Button} from './src/components/Button/Button';
+import {EyeOffIcon} from './src/assets/icons/EyeOffIcon';
+import {Icon} from './src/components/Icon/Icon';
 
 function App(): React.JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
-        <Text preset="headingLarge">asfgsafaasfas</Text>
-        <Text preset="headingLarge">asfgsafaasfas</Text>
-        <Button title="Teste" disabled />
-        <Button title="Teste" preset="outline" disabled />
+        <Icon name="camera" color="carrotSecondary" size={50} />
       </SafeAreaView>
     </ThemeProvider>
   );
